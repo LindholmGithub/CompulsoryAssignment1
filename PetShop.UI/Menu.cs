e1aa44626e646ac13f5f0a3399d37e2078bcc315
Christian Lindholm <christianlindholm@jubii.dk>
2021-08-25 12:19:37 +0200

    akjfnkdsjnsfksnksjgmfklg

--- PetShop.UI/Menu.cs
+++ PetShop.UI/Menu.cs
@@ -1,7 +1,45 @@
+using System;
+using PetShop.Domain.IServices;
+
 namespace PetShop.UI
 {
     public class Menu
     {
-        
+        private IPetService _petService;
+
+        public Menu(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+        public void Start()
+        {
+            StartLoop();
+        }
+
+        private void StartLoop()
+        {
+            int choice;
+            while ((choice = GetMainMenuSelection()) != 0)
+            {
+                
+            }
+        }
+        private int GetMainMenuSelection()
+        {
+            ShowMainMenu();
+            var selectionString = Console.ReadLine();
+            int selection;
+            if (int.TryParse(selectionString, out selection))
+            {
+                return selection;
+            }
+            return -1;
+        }
+        private void ShowMainMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(StringConstants.PrintPetListText);
+        }
     }
 }
---
+++ PetShop.UI/Program.cs
@@ -0,0 +1,22 @@
+
+using PetShop.Infrastructure.Repositories;
+
+namespace PetShop.UI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IPetRepository repoOne = new PetShopRepository();
+            IPetService serviceOne = new PetService(repoOne);
+            
+            var menu = new Menu(serviceOne, serviceTwo);
+            menu.Start();
+            //Cheapish DI (Dependency Injection)
+            //IVideoRepository repo = new VideoRepositoryInMemory();
+            //IVideoService service = new VideoService(repo);
+            //var menu = new Menu(service);
+            //menu.Start();
+        }
+    }
+}
